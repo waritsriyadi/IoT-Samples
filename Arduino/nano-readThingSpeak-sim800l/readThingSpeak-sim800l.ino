894862eb1ce18fe602d52db89cfeb66a382b547e
MushySussy <125733071+waritsriyadi@users.noreply.github.com>
2023-12-21 03:25:47 +0700

    Update readThingSpeak-sim800l.ino

--- Arduino/nano-readThingSpeak-sim800l/readThingSpeak-sim800l.ino
+++ Arduino/nano-readThingSpeak-sim800l/readThingSpeak-sim800l.ino
@@ -1,174 +1,86 @@
 #define TINY_GSM_MODEM_SIM800
-
 #include <TinyGsmClient.h>
-#include <ArduinoHttpClient.h>
 #include <SoftwareSerial.h>
-#include <HardwareSerial.h>
 
-SoftwareSerial sim800lSerial(10, 11);
+// Your ThinkSpeak channel details
+String channelId = "2381662";
+String readApiKey = "YRCSLQV7MLIE2EQE";
 
-const char THINKSPEAK_HOST[] = "api.thingspeak.com";
-const String THINKSPEAK_API_KEY = "YOUR_THINGSPEAK_API_KEY";
-const String THINKSPEAK_CHANNEL_ID = "YOUR_THINGSPEAK_CHANNEL_ID";
-const int SSL_PORT = 443;
+// TinyGsmClient sim800l
+#define APN "m2mautotronic"
+#define SIM800_RX 10
+#define SIM800_TX 11
+int PIN_BUZZER = A5;
+SoftwareSerial sim800_serial(SIM800_RX, SIM800_TX);
+TinyGsm gsm_client(sim800_serial);
+TinyGsmClient client(gsm_client);
 
-char apn[] = "m2mautotronic";
-char user[] = "";
-char pass[] = "";
-
-TinyGsm modem(sim800lSerial);  // For ESP32, Serial2 is used for hardware serial
-TinyGsmClientSecure gsm_client_secure_modem(modem, 0);
-HttpClient http_client = HttpClient(gsm_client_secure_modem, THINKSPEAK_HOST, SSL_PORT);
-
-unsigned long previousMillis = 0;
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("esp32 serial initialize");
-
-  modem.begin(9600, SWSERIAL_8N1, 10, 11, false, 256);  // RX, TX
-  Serial.println("SIM800L serial initialize");
-
-  modem.begin(9600, SERIAL_8N1, 16, 17);  // RX2, TX2 for ESP32
-  sim800lSerial.println("Serial2 (for SIM800) initialize");
-
-  delay(3000);
-
-  // Restart takes quite some time
-  // To skip it, call init() instead of restart()
-  Serial.println("Initializing modem...");
-  modem.init();
-  String modemInfo = modem.getModemInfo();
-  Serial.print("Modem: ");
-  Serial.println(modemInfo);
-
-  // Unlock your SIM card with a PIN
-  // modem.simUnlock("1234");
-
-  http_client.setHttpResponseTimeout(10 * 1000);  // 10 secs timeout
-}
-
-
-void loop() {
-  Serial.print(F("Connecting to "));
-  Serial.print(apn);
-  if (!modem.gprsConnect(apn, user, pass)) {
-    Serial.println(" fail");
-    delay(1000);
-    return;
-  }
-  Serial.println(" OK");
-
-  http_client.connect(THINKSPEAK_HOST, SSL_PORT);
-
-  while (true) {
-    if (!http_client.connected()) {
-      Serial.println();
-      http_client.stop();  // Shutdown
-      Serial.println("HTTP not connected");
-      break;
-    } else
-      readFromThingSpeakField1(&http_client);
-  }
-}
-//**************************************************************************************************
-
-//**************************************************************************************************
-void PostToFirebase(const char* method, const String & path , const String & data, HttpClient* http) {
-  String response;
-  int statusCode = 0;
-  http->connectionKeepAlive(); // Currently, this is needed for HTTPS
-  
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-  String url;
-  if (path[0] != '/') {
-    url = "/";
-  }
-  url += path + ".json";
-  url += "?auth=" + FIREBASE_AUTH;
-  Serial.print("POST:");
-  Serial.println(url);
-  Serial.print("Data:");
-  Serial.println(data);
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-  
-  String contentType = "application/json";
-  http->put(url, contentType, data);
-  
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-  // read the status code and body of the response
-  //statusCode-200 (OK) | statusCode -3 (TimeOut)
-  statusCode = http->responseStatusCode();
-  Serial.print("Status code: ");
-  Serial.println(statusCode);
-  response = http->responseBody();
-  Serial.print("Response: ");
-  Serial.println(response);
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-  if (!http->connected()) {
-    Serial.println();
-    http->stop();// Shutdown
-    Serial.println("HTTP POST disconnected");
-  }
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-}
-//**************************************************************************************************
-
-
-//**************************************************************************************************
-void gps_loop()
-{
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-  //Can take up to 60 seconds
-  boolean newData = false;
-  for (unsigned long start = millis(); millis() - start < 2000;){
-    while (neogps.available()){
-      if (gps.encode(neogps.read())){
-        newData = true;
-        break;
-      }
+void readFromThinkSpeak() {
+  if (client.connected() && client.available()) {
+    String line = client.readStringUntil('\r');
+    Serial.println(line);
+    if (line == "1") {
+      digitalWrite(PIN_BUZZER, HIGH);
+      Serial.println("Buzzer Nyala");
+    } else if (line == "0") {
+      digitalWrite(PIN_BUZZER, LOW);
+      Serial.println("Buzzer Mati");
+    } else {
+      Serial.println("Bukan status normal");
     }
   }
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
 
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
-  //If newData is true
-  if(true){
-  newData = false;
-  
-  String latitude, longitude;
-  //float altitude;
-  //unsigned long date, time, speed, satellites;
-  
-  latitude = String(gps.location.lat(), 6); // Latitude in degrees (double)
-  longitude = String(gps.location.lng(), 6); // Longitude in degrees (double)
-  
-  //altitude = gps.altitude.meters(); // Altitude in meters (double)
-  //date = gps.date.value(); // Raw date in DDMMYY format (u32)
-  //time = gps.time.value(); // Raw time in HHMMSSCC format (u32)
-  //speed = gps.speed.kmph();
-  
-  Serial.print("Latitude= "); 
-  Serial.print(latitude);
-  Serial.print(" Longitude= "); 
-  Serial.println(longitude);
-      
-  String gpsData = "{";
-  gpsData += "\"lat\":" + latitude + ",";
-  gpsData += "\"lng\":" + longitude + "";
-  gpsData += "}";
+  // Check if client is disconnected
+  if (!client.connected() && !client.available()) {
+    Serial.println();
+    Serial.println("Disconnecting from ThinkSpeak...");
+    client.stop();
+    Serial.println("Disconnected from ThinkSpeak");
 
-  //PUT   Write or replace data to a defined path, like messages/users/user1/<data>
-  //PATCH   Update some of the keys for a defined path without replacing all of the data.
-  //POST  Add to a list of data in our Firebase database. Every time we send a POST request, the Firebase client generates a unique key, like messages/users/<unique-id>/<data>
-  //https://firebase.google.com/docs/database/rest/save-data
-  
-  PostToFirebase("PATCH", FIREBASE_PATH, gpsData, &http_client);
-  
+    // Delay before reconnecting
+    delay(10000);
 
+    // Reconnect to ThinkSpeak
+    Serial.println("Reconnecting to ThinkSpeak...");
+    if (client.connect("api.thingspeak.com", 80)) {
+      Serial.println("Connected to ThinkSpeak");
+
+      // Make a request for the last entry
+      String getRequest = "GET /channels/" + channelId + "/fields/1/last.txt?api_key=" + readApiKey;
+      client.println(getRequest);
+      client.println("Host: api.thingspeak.com");
+      client.println("Connection: close");
+      client.println();
+    } else {
+      Serial.println("Failed to reconnect to ThinkSpeak");
+    }
   }
-  //NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
 }
-//**************************************************************************************************
+
+void setup() {
+  Serial.begin(9600);
+  sim800_serial.begin(9600);
+  pinMode(PIN_BUZZER, OUTPUT);
+  gsm_client.init();
+  gsm_client.waitForNetwork();
+  gsm_client.gprsConnect(APN, "", "");
+
+  Serial.println("Connecting to ThinkSpeak...");
+  if (client.connect("api.thingspeak.com", 80)) {
+    Serial.println("Connected to ThinkSpeak");
+
+    // Make a request for the last entry
+    String getRequest = "GET /channels/" + channelId + "/fields/1/last.txt?api_key=" + readApiKey;
+    client.println(getRequest);
+    client.println("Host: api.thingspeak.com");
+    client.println("Connection: close");
+    client.println();
+  } else {
+    Serial.println("Failed to connect to ThinkSpeak");
+  }
+}
+
+void loop() {
+  // Call the readFromThinkSpeak function
+  readFromThinkSpeak();
+}
